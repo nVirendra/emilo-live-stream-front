7155a75772abdea1203c5a2c8af2e816b7660d76
Virendra Nishad <59563175+nVirendra@users.noreply.github.com>
2025-07-28 12:18:51 +0530

    login base url fix

--- src/features/api/api.js
+++ src/features/api/api.js
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
+import { API_URL } from '../../utils/constants';
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api',
+    baseUrl: API_URL,
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
